f079f9d5ec635aec4a198b04aba1ef3a06344080
Pruthviraj Kalashetty <109294152+pruthviraj7171@users.noreply.github.com>
2026-07-08 14:04:03 +0530

    Add full adder implementation using half adders
    Implemented a full adder using two half adders in Verilog.

---
+++ 03-Combinational-Logic/03-Adder-Subctractor.md/Rtl/full_adder_using_two_half_adder.v
@@ -0,0 +1,25 @@
+// Code Full Adder Using Half Adder
+
+//HAlf Adder
+module half_adder(
+  input a,b,
+  output sum,carry
+);
+  assign sum = a & b;
+  assign carry = a ^ b;
+endmodule  
+
+//Full Adder
+module full_adder(
+  input a,b,cin,
+  output sum,carry
+);
+  
+  wire w1,w2,w3;
+  
+//Two HAlf Adder
+  half_adder h1(.a(a), .b(b), .sum(w1), .carry(w2));
+  half_adder h2 (.a(w1), .b(cin), .sum(sum), .carry(w3));
+  
+  or o1(cary,w2,w3);
+  endmodule
