84601320fc4bbbb5934f7322c0108babdad35537
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-12 20:44:34 +0530

    added nor gate verilog and testbench

---
+++ Verilog-HDL-and-RTL-Design/08-Combinational-RTL-Design/01-Logic-Gates/05-NOR-Gate/nor_gate.v
@@ -0,0 +1,11 @@
+module nor_gate(
+    
+    input A;
+    input B;
+    output Y
+
+);
+
+assign Y = ~(A | B);
+
+endmodule
---
+++ Verilog-HDL-and-RTL-Design/08-Combinational-RTL-Design/01-Logic-Gates/05-NOR-Gate/nor_gate_tb.v
@@ -0,0 +1,42 @@
+module nor_gate_tb;
+     
+     reg A,
+     reg B,
+     wire Y,
+
+     nor_gate DUT(
+        .A(A),
+        .B(B),
+        .Y(Y)
+     );
+
+     initial begin
+
+        $dumpfile("nor_gate.vcd");
+        $dumpvars(0, nor_gate_tb);
+
+        A = 0;
+        B = 0;
+
+        #10;
+
+        A = 0;
+        B = 1;
+
+        #10;
+
+        A = 1;
+        B = 0;
+
+        #10;
+
+        A = 1;
+        B = 1;
+
+        #10;
+
+        $finish;
+
+    end    
+
+endmodule
