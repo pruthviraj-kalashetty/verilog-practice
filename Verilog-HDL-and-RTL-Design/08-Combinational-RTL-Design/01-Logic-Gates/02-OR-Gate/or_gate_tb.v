929c97032b8e6676d30bb337c63ee22184603a10
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-12 17:16:23 +0530

    added or testbench

---
+++ Verilog-HDL-and-RTL-Design/08-Combinational-RTL-Design/01-Logic-Gates/02-OR-Gate/or_gate_tb.v
@@ -0,0 +1,39 @@
+module or_gate_tb.v;
+
+     reg A;
+     reg B;
+     wire Y
+
+     and_gate DUT(
+        .A(A),
+        .B(B),
+        .Y(Y),
+    );
+
+    initial begin
+        $dumpfile("and_gate.vcd");
+        $dumpvars(0, and_gate_tb);
+
+        A = 0;
+        B = 0;
+
+        #10;
+
+        A = 0;
+        B = 1;
+
+        #10;
+
+        A = 1;
+        B = 0;
+
+        #10;
+
+        A = 1;
+        B = 1;
+
+        #10;
+
+        $finish;
+    end 
+endmodule        
