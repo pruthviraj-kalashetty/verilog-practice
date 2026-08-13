4df7aa1ea1d9355ac2dbf08ae3b291492b86e567
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-13 23:41:40 +0530

    added full adder testbench

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/02-Adders/02-Full-Adder/full_adder.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/02-Adders/02-Full-Adder/full_adder.v
@@ -2,5 +2,21 @@ module full_adder(
 
     input A,
     input B,
+    input Cin,
+    output sum,
+    output carry
     
-)
+);
+
+wire w1, w2, w3, w4;
+
+assign w1 = A ^ B;
+assign sum = w1 ^ Cin;
+
+assign w2 = A & B;
+assign w3 = B & Cin;
+assign w4 = A & Cin;
+
+assign carry = w2 | w3 | w4;
+
+endmodule
---
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/02-Adders/02-Full-Adder/full_adder_tb.v
@@ -0,0 +1,80 @@
+module full_adder_tb;
+     reg A;
+     reg B;
+     reg Cin;
+
+     wire sum;
+     wire carry;
+
+     full_adder DUT(
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .sum(sum),
+        .carry(carry)
+
+     );
+
+     initial begin
+        $dumpfile("full_adder.vcd");
+        $dumpvars(0, full_adder_tb);
+
+        A = 0;
+        B = 0;
+        Cin = 0;
+
+        #10;
+
+        A = 0;
+        B = 0;
+        Cin = 1;
+
+        #10;
+
+        A = 0;
+        B = 1;
+        Cin = 0;
+
+        #10;
+
+        A = 0;
+        B = 1;
+        Cin = 1;
+
+        #10;
+
+        A = 1;
+        B = 0;
+        Cin = 0;
+         
+        #10;
+
+        A = 1;
+        B = 0;
+        Cin = 1;
+         
+        #10;
+
+        A = 0;
+        B = 1;
+        Cin = 1;
+         
+        #10;
+
+        A = 1;
+        B = 1;
+        Cin = 1;
+         
+        #10;
+
+        A = 1;
+        B = 1;
+        Cin = 1 ;
+         
+        #10;
+
+        $finish;
+
+    end
+
+endmodule
