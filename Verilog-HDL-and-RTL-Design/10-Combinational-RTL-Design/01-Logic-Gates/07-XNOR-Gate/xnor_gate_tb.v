7728878b1c5a38a0f2a623758122d2d4d405be39
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-13 13:29:06 +0530

    added xnor gate verilog

---
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/01-Logic-Gates/07-XNOR-Gate/xnor_gate_tb.v
@@ -0,0 +1,39 @@
+module xnor_gate_tb;
+    reg A;
+    reg B;
+    wire Y;
+
+    xnor_gate DUT(
+        .A(A),
+        .B(B),
+        ,Y(Y)
+
+    );
+
+    initial begin
+        $dumpfile("xnor_gate.vcd");
+        $dumpvars(0, xnor_gate_tb);
+
+        A = 0;
+        B = 0;
+
+        #10;
+
+        A = 0;
+        B = 1;
+
+        #10;
+
+        A = 1;
+        B = 0;
+
+        # 10;
+
+        A = 1;
+        B = 1;
+
+        #10;
+
+    end    
+
+endmodule
