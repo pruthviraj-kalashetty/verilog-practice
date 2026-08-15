ed46416fb56d4f25d8c2d1a92473e2517baad425
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-16 00:19:07 +0530

    added full subtractor

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/03-Subtractor/02-Full-Subtractoe/full_subtractor.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/03-Subtractor/02-Full-Subtractoe/full_subtractor.v
@@ -1,1 +1,22 @@
+module full_subtractor(
+
+    input A,
+    input B,
+    input Bin,
+    output difference,
+    output borrow
+
+);
+
+wire w1, w2, w3;
+
+assign w1 = A ^ B;
+assign w2 = ~A & B;
+
+assign difference = w1 ^ Bin;
+assign w3 = ~w1 & Bin;
+
+assign borrow = w2 | w3;
+
+endmodule
 
