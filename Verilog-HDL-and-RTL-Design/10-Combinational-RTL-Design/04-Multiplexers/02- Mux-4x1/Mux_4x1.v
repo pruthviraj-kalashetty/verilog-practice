4051a0d6e2ee07516c25548d2aebb44b8f8339e2
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-16 23:18:07 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/04-Multiplexers/02- Mux-4x1/Mux_4x1.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/04-Multiplexers/02- Mux-4x1/Mux_4x1.v
@@ -1,1 +1,25 @@
+module mux_4to1(
 
+    input I0,
+    input I1,
+    input I2,
+    input I3,
+
+    input S0,
+    input S1,
+
+    output reg Y
+);
+
+always @(*) begin
+    
+    case ({S1, S0})
+
+       2'b00: Y = I0;
+       2'b01: Y = I1;
+       2'b10: Y = I2;
+       2'b11: Y = I3;
+
+    endcase
+end
+endmodule       
