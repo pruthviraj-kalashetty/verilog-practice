f0f5c688a8e1cf8006342711331411e697c6d5c7
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-16 15:47:00 +0530

    added testbench

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/03-Subtractor/03-Full-Subtractor-Using-Two-Half-Subtractor/half_subtractor_using_two_half_subtrctor_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/03-Subtractor/03-Full-Subtractor-Using-Two-Half-Subtractor/half_subtractor_using_two_half_subtrctor_tb.v
@@ -1,1 +1,74 @@
+module full_subtractor_using_half_subtractor_tb;
+     reg A;
+     reg B;
+     reg Bin;
 
+     wire difference;
+     wire borrow;
+
+     full_subtractor_using_half_subtractor DUT(
+        .A(A),
+        .B(B),
+        .Bin(Bin),
+        .difference(difference),
+        .borrow(borrow)
+
+     );
+
+     initial begin
+        $dumpfile("full_subtractor_using_half_subtractor.vcd");
+        $dumpvars(0, full_subtractor_using_half_subtractor_tb);
+
+        A = 0;
+        B = 0;
+        Bin = 0;
+
+        #10;
+
+        A = 0;
+        B = 0;
+        Bin = 1;
+
+        #10;
+
+        A = 0;
+        B = 1;
+        Bin = 0;
+
+        #10;
+
+        A = 0;
+        B = 1;
+        Bin = 1;
+
+        #10;
+
+        A = 1;
+        B = 0;
+        Bin = 0;
+         
+        #10;
+
+        A = 1;
+        B = 0;
+        Bin = 1;
+         
+        #10;
+
+        A = 1;
+        B = 1;
+        Bin = 0;
+         
+        #10;
+
+        A = 1;
+        B = 1;
+        Bin = 1;
+         
+        #10;
+
+        $finish;
+
+    end
+
+endmodule
