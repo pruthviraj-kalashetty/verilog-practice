6b34b32fb6807dd2da7dce4d9df68ed397491c8c
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-16 23:22:18 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/04-Multiplexers/02- Mux-4x1/Mux_4x1_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/04-Multiplexers/02- Mux-4x1/Mux_4x1_tb.v
@@ -32,35 +32,35 @@ module mux_4to1_tb;
         // Input value
 
         I0 = 0;
-        I1 = l;
+        I1 = 1;
         I2 = 0;
         I3 = 1;
 
-        // S0S1 = 00  select I0
+        // S1S0 = 00  select I0
 
-        S0 = 0;
         S1 = 0;
+        S0 = 0;
 
         #10;
         
-        // S0S1 = 01  select I1
+        // S1S0 = 01  select I1
 
-        S0 = 0;
-        S1 = 1;
-
-        #10;
-
-        // S0S1 = 10 select I2
-
-        S0 = 1;
         S1 = 0;
+        S0 = 1;
 
         #10;
 
-        // S0S1 = 11  select I3
+        // S1S0 = 10 select I2
 
-        S0 = I1;
-        S1 = I2;
+        S1 = 1;
+        S0 = 0;
+
+        #10;
+
+        // S1S0 = 11  select I3
+
+        S1 = 1;
+        S0 = 1;
 
         #10;
 
