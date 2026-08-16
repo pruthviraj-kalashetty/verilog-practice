e8781c73a0d8136b29855a00c034fb15f09651fd
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-16 15:39:28 +0530

    added verilog

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/03-Subtractor/03-Full-Subtractor-Using-Two-Half-Subtractor/half_subtractor_using_two_half_subtrctor.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/03-Subtractor/03-Full-Subtractor-Using-Two-Half-Subtractor/half_subtractor_using_two_half_subtrctor.v
@@ -1,1 +1,20 @@
+module full_subtractor_using_two_half_subtractor(
 
+    input A,
+    input B,
+    input Bin,
+    output difference,
+    output borrow
+
+);
+
+wire w1, w2, w3;
+
+assign w1 = A ^ B;
+assign difference = w1 ^ Bin;
+
+assign w2 = ~A & B;
+assign w3 = ~w1 & Bin;
+assign borrow = w2 | w3;
+
+endmodule
