dd46ac3acfab8ede8c73ebbfe48c54d8ce5e959a
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-16 19:36:13 +0530

    added mux verilog

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/04-Multiplexers/01- Mux-2x1/mux_2x1.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/04-Multiplexers/01- Mux-2x1/mux_2x1.v
@@ -1,1 +1,17 @@
+module mux_2to1(
+    
+    input I0,
+    input I1,
+    input S,
+    output Y
 
+);
+
+wire w1, w2;
+
+assign w1 = ( I0 & ~S);
+assign w2 = ( I1 & S);
+
+assign Y = w1 | w2;
+
+endmodule
