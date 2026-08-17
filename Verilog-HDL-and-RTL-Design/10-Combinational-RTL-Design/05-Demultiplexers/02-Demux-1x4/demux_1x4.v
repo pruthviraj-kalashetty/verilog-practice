0dce58532fc7effc45053251dad6388cfaaaf944
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-17 23:00:49 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/05-Demultiplexers/02-Demux-1x4/demux_1x4.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/05-Demultiplexers/02-Demux-1x4/demux_1x4.v
@@ -1,1 +1,44 @@
+module demux_1to4(
 
+    input D,
+    input S1,S0,
+
+    output reg Y3, Y2, Y1, Y0
+
+);
+
+always @(*) begin 
+   case ({S1, S0})
+
+   2'b00: begin
+      Y0 = D;
+      Y1 = 0;
+      Y2 = 0;
+      Y3 = 0;
+
+   end
+
+   2'b01: begin 
+      Y0 = 0;
+      Y1 = D;
+      Y2 = 0;
+      Y3 = 0;
+   end
+
+   2'b10: begin 
+      Y0 = 0;
+      Y1 = 0;
+      Y2 = D;
+      Y3 = 0;
+   end
+
+   2'b11: begin 
+      Y0 = 0;
+      Y1 = 0;
+      Y2 = 0;
+      Y3 = D;
+   end   
+
+   endcase
+end
+endmodule   
