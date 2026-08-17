9241384eb4411ede96633c65a76f7024bc269c41
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-17 14:30:57 +0530

    added testbench

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/05-Demultiplexers/01-Demux-1x2/demux_1x2_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/05-Demultiplexers/01-Demux-1x2/demux_1x2_tb.v
@@ -1,1 +1,49 @@
+module demux_1to2_tb.v;
+    
+    reg D;
+    reg S;
 
+    wire Y0;
+    wire Y1;
+
+    demux_1to2 DUT(.*);
+
+    initial begin
+      $dumpfile("demux_1to2.vcd");
+      $dumpvars(0, demux_1to2_tb);
+
+      // D = 0, S = 0 → Y0 = 0, Y1 = 0
+
+        D = 0;
+        S = 0;
+
+        #10;
+
+        // D = 0, S = 1 → Y0 = 0, Y1 = 0
+
+        D = 0;
+        S = 1;
+
+        #10;
+
+        // D = 1, S = 0 → D goes to Y0
+
+        D = 1;
+        S = 0;
+
+        #10;
+
+        // D = 1, S = 1 → D goes to Y1
+
+        D = 1;
+        S = 1;
+
+        #10;
+
+        $finish;
+
+    end
+
+endmodule
+
+    
