f9ca2ac5c108258c12462c740f27dccf03b9c560
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-19 20:54:22 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/06-Decoders/02-Decoder-3x8/decoder_3x8_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/06-Decoders/02-Decoder-3x8/decoder_3x8_tb.v
@@ -1,1 +1,50 @@
+module decoder_3to8_tb;
+    reg [2:0] A;
 
+    wire [7:0] Y;
+
+    decoder_3to8 DUT(
+        .A(A),
+        .Y(Y)
+    );
+
+  initial begin
+     $dumpfile("decoder_3to8.vcd");
+     $dumpvars(0, decoder_3to8_tb);
+
+     A = 3'b000;
+
+     #10;
+
+     A = 3'b001;
+
+     #10;
+
+     A = 3'b010;
+
+     #10;
+
+     A = 3'b011;
+
+     #10;
+
+     A = 3'b100;
+
+     #10;
+
+     A = 3'b101;
+
+     #10;
+
+     A = 3'b110;
+
+     #10;
+
+     A = 3'b111;
+
+     #10;
+
+     $finish;
+  
+    end
+endmodule    
