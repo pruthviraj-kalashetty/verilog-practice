cad348e69794e098d2e22528758c154a8e075756
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-19 19:58:21 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/06-Decoders/01-Decoder-2x4/decoder_2x4_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/06-Decoders/01-Decoder-2x4/decoder_2x4_tb.v
@@ -1,1 +1,51 @@
+module decoder_2to4_tb.v;
 
+    reg A, B;
+    
+    wire[3:0]  Y;
+
+
+    decoder_2to4 DUT(
+        .A(A),
+        .B(B),
+        .Y(Y)
+
+    );
+
+    initial begin 
+      $dumpfile("decoder_2to4.vcd");
+      $dumpvars(0, decoder_2to4_tb);
+      
+      // A B = 00 → Y = 0001
+
+      A = 0;
+      B = 0;
+
+      #10;
+
+      // A B = 01 → Y = 0010
+
+      A = 0;
+      B = 1;
+
+      // A B = 10 → Y = 0100
+
+      #10;
+
+      A = 1;
+      B = 0;
+
+      #10;
+
+      // A B = 11 → Y = 1000
+
+      A = 1;
+      B = 1;
+
+      #10;
+
+      $finish;
+
+    end
+
+endmodule      
