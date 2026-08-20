5d83d5dc917a513ac98e1ed18c3170dbcd96b53b
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-20 15:34:15 +0530

    added verilog

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/07-Encoders/01-Encoder-4x2/encoder_4x2.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/07-Encoders/01-Encoder-4x2/encoder_4x2.v
@@ -1,1 +1,20 @@
+module encoder_4to2.v(
 
+    input [3:0] A,
+    output [1:0] Y
+
+);
+
+always @(*) begin
+     
+     case (A)
+
+        4'b0001: Y = 2'b00;
+        4'b0010: Y = 2'b01;
+        4'b0100: Y = 2'b10;
+        4'b1000: Y = 2'b11;
+
+    endcase
+
+end
+endmodule        
