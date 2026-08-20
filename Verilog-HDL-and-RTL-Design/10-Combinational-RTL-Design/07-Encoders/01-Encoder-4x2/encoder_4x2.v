e24127fbbfbd03141b77405e1cfcf515699db3fc
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-20 16:18:48 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/07-Encoders/01-Encoder-4x2/encoder_4x2.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/07-Encoders/01-Encoder-4x2/encoder_4x2.v
@@ -6,16 +6,18 @@ module encoder_4to2(
 );
 
 always @(*) begin
-     
-     case (A)
+
+    case (A)
 
         4'b0001: Y = 2'b00;
         4'b0010: Y = 2'b01;
         4'b0100: Y = 2'b10;
         4'b1000: Y = 2'b11;
-        
+
         default: Y = 2'b00;
+
     endcase
 
 end
-endmodule        
+
+endmodule
--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/07-Encoders/01-Encoder-4x2/encoder_4x2_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/07-Encoders/01-Encoder-4x2/encoder_4x2_tb.v
@@ -13,28 +13,16 @@ module encoder_4to2_tb;
         $dumpfile("encoder_4to2.vcd");
         $dumpvars(0, encoder_4to2_tb);
 
-        // A = 0001 → Y = 00
-
         A = 4'b0001;
-
         #10;
-
-        // A = 0010 → Y = 01
 
         A = 4'b0010;
-
         #10;
-
-        // A = 0100 → Y = 10
 
         A = 4'b0100;
-
         #10;
 
-        // A = 1000 → Y = 11
-
         A = 4'b1000;
-
         #10;
 
         $finish;
