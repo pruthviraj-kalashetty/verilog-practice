710388f10529d1924e4217720742c29040824fb7
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-21 16:43:51 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/1-Bit-Comparator/1_bit_comparator.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/1-Bit-Comparator/1_bit_comparator.v
@@ -1,1 +1,39 @@
+module one_bit_comparator(
 
+    input A, B,
+
+    output reg A_greater_B,
+    output reg A_equal_B,
+    output reg A_less_B
+
+);
+
+always @(*) begin
+
+    if (A > B) begin
+
+        A_greater_B = 1;
+        A_equal_B = 0;
+        A_less_B = 0;
+
+    end
+
+    else if (A == B) begin
+
+        A_greater_B = 0;
+        A_equal_B = 1;
+        A_less_B = 0;
+
+    end
+
+    else begin
+
+        A_greater_B = 0;
+        A_equal_B = 0;
+        A_less_B = 1;
+
+    end
+
+end
+
+endmodule
--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/1-Bit-Comparator/1_bit_comparator_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/1-Bit-Comparator/1_bit_comparator_tb.v
@@ -1,1 +1,52 @@
+module encoder_16to4_tb;
+   reg A, B;
+   wire A_greater_B;
+   wire A_equal_B;
+   wire A_less_B;
 
+   encoder_16to4 DUT(
+    .A(A),
+    .B(A),
+    .A_greater_B(A_greater_B),
+    .A_equal_B(A_equal_B),
+    .A_less_B(A_less_B)
+   );
+
+   initial begin
+     $dumpfile("encoder_16to4.vcd");
+     $dumpvars(0, encoder_16to4_tb);
+
+     
+        // A = 0, B = 0 → A = B
+
+        A = 0;
+        B = 0;
+
+        #10;
+
+        // A = 0, B = 1 → A < B
+
+        A = 0;
+        B = 1;
+
+        #10;
+
+        // A = 1, B = 0 → A > B
+
+        A = 1;
+        B = 0;
+
+        #10;
+
+        // A = 1, B = 1 → A = B
+
+        A = 1;
+        B = 1;
+
+        #10;
+
+        $finish;
+
+    end
+
+endmodule 
