1a8d6b96d906057542133348d980c53d537ff819
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-21 20:58:28 +0530

    updated

--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/4-Bit-Comparator/4_bit_comparator.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/4-Bit-Comparator/4_bit_comparator.v
@@ -1,1 +1,36 @@
+module four_bit_comparator(
+    input A0, A1, A2, A3, B0, B1, B2, B3,
 
+    output reg A_greater_B,
+    output reg A_equal_B,
+    output reg A_less_B 
+);
+
+always @(*) begin
+      
+    if ({A3, A2, A1, A0} > {B3, B2, B1, B0,}) begin
+
+         A_greater_B = 1;
+         A_equal_B = 0;
+         A_less_B = 0;
+
+    end
+
+    else if ({A3, A2, A1, A0} == {B3, B2, B1, B0}) begin
+
+         A_greater_B = 0;
+         A_equal_B = 1;
+         A_less_B = 0;
+
+    end
+
+    else begin 
+
+         A_greater_B = 0;
+         A_equal_B = 0;
+         A_less_B = 1;
+
+    end
+
+end 
+endmodule         
--- Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/4-Bit-Comparator/4_bit_comparator_tb.v
+++ Verilog-HDL-and-RTL-Design/10-Combinational-RTL-Design/08-Comparators/4-Bit-Comparator/4_bit_comparator_tb.v
@@ -1,1 +1,147 @@
+module four_bit_comparator_tb;
 
+    reg A0, A1, A2, A3;
+    reg B0, B1, B2, B3;
+
+    wire A_greater_B;
+    wire A_equal_B;
+    wire A_less_B;
+
+    four_bit_comparator DUT(
+        .A0(A0),
+        .A1(A1),
+        .A2(A2),
+        .A3(A3),
+
+        .B0(B0),
+        .B1(B1),
+        .B2(B2),
+        .B3(B3),
+
+        .A_greater_B(A_greater_B),
+        .A_equal_B(A_equal_B),
+        .A_less_B(A_less_B)
+    );
+
+    initial begin
+
+        $dumpfile("four_bit_comparator.vcd");
+        $dumpvars(0, four_bit_comparator_tb);
+
+        // A = 0000, B = 0000 → A = B
+
+        A3 = 0;
+        A2 = 0;
+        A1 = 0;
+        A0 = 0;
+
+        B3 = 0;
+        B2 = 0;
+        B1 = 0;
+        B0 = 0;
+
+        #10;
+
+        // A = 0001, B = 0000 → A > B
+
+        A3 = 0;
+        A2 = 0;
+        A1 = 0;
+        A0 = 1;
+
+        B3 = 0;
+        B2 = 0;
+        B1 = 0;
+        B0 = 0;
+
+        #10;
+
+        // A = 0000, B = 0001 → A < B
+
+        A3 = 0;
+        A2 = 0;
+        A1 = 0;
+        A0 = 0;
+
+        B3 = 0;
+        B2 = 0;
+        B1 = 0;
+        B0 = 1;
+
+        #10;
+
+        // A = 1010, B = 0111 → A > B
+
+        A3 = 1;
+        A2 = 0;
+        A1 = 1;
+        A0 = 0;
+
+        B3 = 0;
+        B2 = 1;
+        B1 = 1;
+        B0 = 1;
+
+        #10;
+
+        // A = 0101, B = 0101 → A = B
+
+        A3 = 0;
+        A2 = 1;
+        A1 = 0;
+        A0 = 1;
+
+        B3 = 0;
+        B2 = 1;
+        B1 = 0;
+        B0 = 1;
+
+        #10;
+
+        // A = 0011, B = 1000 → A < B
+
+        A3 = 0;
+        A2 = 0;
+        A1 = 1;
+        A0 = 1;
+
+        B3 = 1;
+        B2 = 0;
+        B1 = 0;
+        B0 = 0;
+
+        #10;
+
+        // A = 1111, B = 0000 → A > B
+
+        A3 = 1;
+        A2 = 1;
+        A1 = 1;
+        A0 = 1;
+
+        B3 = 0;
+        B2 = 0;
+        B1 = 0;
+        B0 = 0;
+
+        #10;
+
+        // A = 0000, B = 1111 → A < B
+
+        A3 = 0;
+        A2 = 0;
+        A1 = 0;
+        A0 = 0;
+
+        B3 = 1;
+        B2 = 1;
+        B1 = 1;
+        B0 = 1;
+
+        #10;
+
+        $finish;
+
+    end
+
+endmodule
