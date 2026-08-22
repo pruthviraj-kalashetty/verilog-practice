5642721ed0c3fbf89a4fcc563a9c43456768847c
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-22 16:43:12 +0530

    added testbench

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/01-Latches/02-D-Latch/d_latch_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/01-Latches/02-D-Latch/d_latch_tb.v
@@ -1,1 +1,35 @@
+module d_latch_tb;
+  reg D, EN;
 
+  wire Q;
+  wire Q_bar;
+
+  d_latch DUT(
+    .D(D),
+    .EN(EN),
+    .Q(Q),
+    .Q_bar(Q_bar)    
+  );
+
+  initial begin
+    $dumpfile("d_latch");
+    $dumpvars(0, d_latch_tb);
+
+    EN = 0;
+    D = 0;
+
+    #10;
+
+    EN = 1;
+    D = 0;
+
+    #10;
+
+    EN = 1;
+    D = 1;
+
+    #10;
+
+    $finish;
+  end
+endmodule    
