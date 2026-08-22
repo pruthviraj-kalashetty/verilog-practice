0f640c1daed8d5e099747f96ccaec473fe12d620
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-22 14:19:38 +0530

    added sr latch verilog

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/01-Latches/01-SR-Latch/sr_latch.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/01-Latches/01-SR-Latch/sr_latch.v
@@ -1,1 +1,16 @@
+module sr_latch(
+    input S, R,
 
+    output reg Q
+
+);
+
+    assign @(*) begin
+      
+      if (S)
+        Q = 1'b1;
+
+      else if (R)
+        Q = 1'b0;
+    end
+endmodule          
