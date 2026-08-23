bb8ee5f0df2291db2a417d976ef57d71bc658868
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-23 15:21:26 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/03-SR-Flip-Flop/sr_flip_flop.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/03-SR-Flip-Flop/sr_flip_flop.v
@@ -1,1 +1,28 @@
+module sr_flip_flop(
 
+    input S, R, CLK,
+
+    output reg Q,
+    output Q_bar
+
+);
+
+assign Q_bar = ~Q;
+
+always @(posedge CLK) begin
+
+    if (S == 0 && R == 0)
+        Q <= Q;
+
+    else if (S == 0 && R == 1)
+        Q <= 0;
+
+    else if (S == 1 && R == 0)
+        Q <= 1;
+
+    else
+        Q <= 1'bx;
+
+end
+
+endmodule
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/03-SR-Flip-Flop/sr_flip_flop_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/03-SR-Flip-Flop/sr_flip_flop_tb.v
@@ -1,1 +1,72 @@
+module sr_flip_flop_tb;
 
+    reg S, R, CLK;
+
+    wire Q;
+    wire Q_bar;
+
+    sr_flip_flop DUT(
+        .S(S),
+        .R(R),
+        .CLK(CLK),
+        .Q(Q),
+        .Q_bar(Q_bar)
+    );
+
+    // Clock generation
+
+    initial begin
+
+        CLK = 0;
+
+        forever #5 CLK = ~CLK;
+
+    end
+
+
+    initial begin
+
+        $dumpfile("sr_flip_flop.vcd");
+        $dumpvars(0, sr_flip_flop_tb);
+
+
+        // 10 → SET
+        S = 1;
+        R = 0;
+
+        #10;
+
+
+        // 00 → HOLD
+        S = 0;
+        R = 0;
+
+        #10;
+
+
+        // 01 → RESET
+        S = 0;
+        R = 1;
+
+        #10;
+
+
+        // 00 → HOLD
+        S = 0;
+        R = 0;
+
+        #10;
+
+
+        // 10 → SET again
+        S = 1;
+        R = 0;
+
+        #10;
+
+
+        $finish;
+
+    end
+
+endmodule
