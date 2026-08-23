ef17f5a7f182542e17bd9140bc642474d9b0f422
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-23 15:47:27 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/04-T-Flip-Flop/t_flip_flop.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/04-T-Flip-Flop/t_flip_flop.v
@@ -1,1 +1,22 @@
+module t_flip_flop(
 
+    input T, CLK,
+
+    output reg Q,
+    output Q_bar
+
+);
+
+assign Q_bar = ~Q;
+
+always @(posedge CLK) begin
+
+    if (T == 0)
+        Q <= Q;
+
+    else
+        Q <= ~Q;
+
+end
+
+endmodule
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/04-T-Flip-Flop/t_flip_flop_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/04-T-Flip-Flop/t_flip_flop_tb.v
@@ -1,1 +1,49 @@
+module t_flip_flop_tb;
 
+    reg T, CLK;
+
+    wire Q;
+    wire Q_bar;
+
+    t_flip_flop DUT(
+        .T(T),
+        .CLK(CLK),
+        .Q(Q),
+        .Q_bar(Q_bar)
+    );
+
+    // Clock generation
+
+    initial begin
+
+        CLK = 0;
+
+        forever #5 CLK = ~CLK;
+
+    end
+
+    initial begin
+
+        $dumpfile("t_flip_flop.vcd");
+        $dumpvars(0, t_flip_flop_tb);
+
+        // T = 1  TOGGLE
+        T = 1;
+
+        #20;
+
+        // T = 0  HOLD
+        T = 0;
+
+        #20;
+
+        // T = 1  TOGGLE
+        T = 1;
+
+        #20;
+
+        $finish;
+
+    end
+
+endmodule
