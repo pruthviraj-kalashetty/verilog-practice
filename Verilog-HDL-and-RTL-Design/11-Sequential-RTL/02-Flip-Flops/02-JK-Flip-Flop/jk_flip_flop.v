d56d24dda5780944765fac212ea16b66f482f11a
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-23 12:39:44 +0530

    added verilog

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/02-JK-Flip-Flop/jk_flip_flop.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/02-Flip-Flops/02-JK-Flip-Flop/jk_flip_flop.v
@@ -1,1 +1,26 @@
+module jk_flip_flop(
 
+    input J, K, CLK,
+    output reg Q,
+    output Q_bar
+
+);
+
+assign Q_bar = ~Q;
+
+always @(posedge CLK) begin
+
+    if (J == 0 && K == 0)
+         Q <= Q;
+
+    else if (J == 0 && K == 1)
+         Q <= 0;
+
+    else if (J == 1 && K == 0)
+         Q <= 1;
+
+    else 
+         Q <= ~Q;
+
+end 
+endmodule                        
