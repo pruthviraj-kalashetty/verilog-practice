474f2da989e70c5e0acb8e155c803e64787fecdb
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-24 22:16:08 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/02- Serial-In-Parallel-Out/sipo_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/02- Serial-In-Parallel-Out/sipo_tb.v
@@ -1,1 +1,54 @@
+module sipo_tb;
 
+reg CLK;
+reg clear;
+reg SI;
+
+wire [3:0] Q;
+
+sipo DUT(
+    .CLK(CLK),
+    .clear(clear),
+    .SI(SI),
+    .Q(Q)
+);
+
+initial begin
+
+    $dumpfile("sipo.vcd");
+    $dumpvars(0, sipo_tb);
+
+    // Initial values
+    CLK = 0;
+    clear = 1;
+    SI = 0;
+
+    #10;
+
+    // Release clear
+    clear = 0;
+
+    // Send 1
+    SI = 1;
+    #10;
+
+    // Send 0
+    SI = 0;
+    #10;
+
+    // Send 1
+    SI = 1;
+    #10;
+
+    // Send 1
+    SI = 1;
+    #10;
+
+    $finish;
+
+end
+
+// Clock generation
+always #5 CLK = ~CLK;
+
+endmodule
