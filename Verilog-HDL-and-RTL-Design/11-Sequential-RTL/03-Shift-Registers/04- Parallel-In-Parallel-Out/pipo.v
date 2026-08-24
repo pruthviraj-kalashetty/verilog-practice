0e4ab741d970ee0ae7ddf1cc91b8d62e66f8e096
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-24 22:53:53 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/04- Parallel-In-Parallel-Out/pipo.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/04- Parallel-In-Parallel-Out/pipo.v
@@ -1,1 +1,18 @@
+module pipo(
+    input CLK,
+    input clear,
+    input [3:0] D,
+    output reg [3:0] Q
+);
 
+always @(posedge CLK) begin
+
+    if (clear == 1)
+        Q <= 4'b0000;
+
+    else
+        Q <= D;
+
+end
+
+endmodule
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/04- Parallel-In-Parallel-Out/pipo_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/04- Parallel-In-Parallel-Out/pipo_tb.v
@@ -1,1 +1,58 @@
+module pipo_tb;
 
+reg CLK;
+reg clear;
+reg [3:0] D;
+
+wire [3:0] Q;
+
+pipo DUT(
+    .CLK(CLK),
+    .clear(clear),
+    .D(D),
+    .Q(Q)
+);
+
+initial begin
+
+    $dumpfile("pipo.vcd");
+    $dumpvars(0, pipo_tb);
+
+    // Initial values
+    CLK = 0;
+    clear = 1;
+    D = 4'b0000;
+
+    #10;
+
+    // Release clear
+    clear = 0;
+
+    // Load 1010
+    D = 4'b1010;
+
+    #10;
+
+    // Load 1100
+    D = 4'b1100;
+
+    #10;
+
+    // Load 0111
+    D = 4'b0111;
+
+    #10;
+
+    // Clear
+    clear = 1;
+
+    #10;
+
+    $finish;
+
+end
+
+// Clock generation
+always #5 CLK = ~CLK;
+
+endmodule
