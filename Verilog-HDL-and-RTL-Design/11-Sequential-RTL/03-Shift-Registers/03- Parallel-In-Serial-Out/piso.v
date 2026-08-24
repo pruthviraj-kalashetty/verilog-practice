83166815118034eff4253d91411a5d49a33a928f
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-24 22:45:28 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/03- Parallel-In-Serial-Out/piso.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/03- Parallel-In-Serial-Out/piso.v
@@ -1,1 +1,26 @@
+module piso(
+    input CLK,
+    input clear,
+    input load,
+    input [3:0] P,
+    output SO
+);
 
+reg [3:0] Q;
+
+always @(posedge CLK) begin
+
+    if (clear == 1)
+        Q <= 4'b0000;
+
+    else if (load == 1)
+        Q <= P;
+
+    else
+        Q <= {Q[2:0], 1'b0};
+
+end
+
+assign SO = Q[3];
+
+endmodule
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/03- Parallel-In-Serial-Out/piso_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/03-Shift-Registers/03- Parallel-In-Serial-Out/piso_tb.v
@@ -1,1 +1,55 @@
+module piso_tb;
 
+reg CLK;
+reg clear;
+reg load;
+reg [3:0] P;
+
+wire SO;
+
+piso DUT(
+    .CLK(CLK),
+    .clear(clear),
+    .load(load),
+    .P(P),
+    .SO(SO)
+);
+
+initial begin
+
+    $dumpfile("piso.vcd");
+    $dumpvars(0, piso_tb);
+
+    // Initial values
+    CLK = 0;
+    clear = 1;
+    load = 0;
+    P = 4'b0000;
+
+    #10;
+
+    // Release clear
+    clear = 0;
+
+    // Parallel load
+    P = 4'b1011;
+    load = 1;
+
+    #10;
+
+    // Start shifting
+    load = 0;
+
+    #10;
+    #10;
+    #10;
+    #10;
+
+    $finish;
+
+end
+
+// Clock generation
+always #5 CLK = ~CLK;
+
+endmodule
