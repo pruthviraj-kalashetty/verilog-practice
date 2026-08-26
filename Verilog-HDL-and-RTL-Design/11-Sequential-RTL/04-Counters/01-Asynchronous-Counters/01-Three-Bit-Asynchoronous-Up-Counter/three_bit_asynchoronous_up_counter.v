c7e8251263f1bd86e1a3ba39b05e29bafa18cbb5
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-26 16:45:30 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/01-Three-Bit-Asynchoronous-Up-Counter/three_bit_asynchoronous_up_counter.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/01-Three-Bit-Asynchoronous-Up-Counter/three_bit_asynchoronous_up_counter.v
@@ -1,1 +1,17 @@
+module async_3bit_up_counter(
+    input clk,
+    output reg [2:0] Q
+);
 
+always @(posedge clk) begin
+    Q[0] <= ~Q[0];
+end
+
+always @(posedge clk Q[0]) begin
+    Q[1] <= ~Q[1];
+end
+
+always @(posedge clk Q[1]) begin
+    Q[2] <= Q[2];
+end    
+endmodule
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/01-Three-Bit-Asynchoronous-Up-Counter/three_bit_asynchoronous_up_counter_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/01-Three-Bit-Asynchoronous-Up-Counter/three_bit_asynchoronous_up_counter_tb.v
@@ -1,1 +1,27 @@
+module async_3bit_up_counter_tb;
+  reg clk;
+  wire [2:0] Q;
 
+
+    async_3bit_up_counter DUT(
+        .clk(clk),
+        .Q(Q)
+    
+    );
+
+    //Clock 
+    initial begin
+       clk = 0;
+
+    forever #5 clk <= ~clk;
+    end
+
+    initial begin  
+       $dumpfile("async_3bit_up_counter.vcd");
+       $dumpvars(0, async_3bit_up_counter_tb) ;
+
+       #100;
+       $finish;
+
+    end
+endmodule            
