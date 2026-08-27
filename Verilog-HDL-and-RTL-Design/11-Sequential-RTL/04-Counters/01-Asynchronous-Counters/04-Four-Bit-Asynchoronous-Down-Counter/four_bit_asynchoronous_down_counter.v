ae77f16b3241b3f2a3a81e58ee76eac39d970edc
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-27 10:16:44 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/04-Four-Bit-Asynchoronous-Down-Counter/four_bit_asynchoronous_down_counter.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/04-Four-Bit-Asynchoronous-Down-Counter/four_bit_asynchoronous_down_counter.v
@@ -1,1 +1,25 @@
+module async_4bit_down_counter(
+    input clk,
+    output reg [3:0] Q
+);
 
+initial begin
+   Q = 4'b1111;
+end 
+
+always @(posedge clk) begin
+    Q[0] <= ~Q[0];
+end
+
+always @(posedge Q[0]) begin
+    Q[1] <= ~Q[1];
+end    
+
+always @(posedge Q[1]) begin
+    Q[2] <= ~Q[2];
+end
+
+always @(posedge Q[2]) begin
+    Q[3] <= ~Q[3];
+end
+endmodule
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/04-Four-Bit-Asynchoronous-Down-Counter/four_bit_asynchoronous_down_counter_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/04-Four-Bit-Asynchoronous-Down-Counter/four_bit_asynchoronous_down_counter_tb.v
@@ -1,1 +1,26 @@
+module async_4bit_up_counter_tb;
+   reg clk;
+   wire [3:0] Q;
+
+   async_4bit_up_counter DUT(
+    .clk(clk),
+    .Q(Q)
+   );
+
+// Clock
+
+initial begin
+   clk = 0;
+   forever #5 clk <= ~clk;
+end
+
+initial begin 
+  $dumpfile("async_4bit_up_counter.vcd");
+  $dumpvars(0, async_4bit_up_counter_tb);
+
+  #200;
+
+  $finish;
+end
+endmodule       
 
