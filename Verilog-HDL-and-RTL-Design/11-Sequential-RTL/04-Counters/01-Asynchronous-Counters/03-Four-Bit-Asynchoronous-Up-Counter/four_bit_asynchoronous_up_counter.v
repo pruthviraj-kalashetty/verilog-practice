49c37a499a743e567a8e0323864cdb4c1fbc844c
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-27 09:20:24 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/03-Four-Bit-Asynchoronous-Up-Counter/four_bit_asynchoronous_up_counter.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/01-Asynchronous-Counters/03-Four-Bit-Asynchoronous-Up-Counter/four_bit_asynchoronous_up_counter.v
@@ -1,1 +1,26 @@
+module async_4bit_up_counter(
+    input clk,
+    output reg [3:0] Q
+);
 
+initial begin 
+   Q = 4'b0000;
+end
+
+always @(posedge clk) begin
+   Q[0] <= ~Q[0];
+end
+
+always @(negedge Q[0]) begin
+   Q[1] <= ~Q[1];
+end
+
+always @(negedge Q[1]) begin
+   Q[2] <= ~Q[2];
+end
+
+always @(negedge clk Q[2]) begin
+   Q[3] <= ~Q[3];
+end
+
+endmodule
