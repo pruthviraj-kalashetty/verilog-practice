c27b2f9265bad39cede232fc9bdc43a65e4cd589
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-28 10:19:34 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/02-Three-Bit-Synchoronous-Down-Counter/three_bit_synchoronous_down_counter.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/02-Three-Bit-Synchoronous-Down-Counter/three_bit_synchoronous_down_counter.v
@@ -1,1 +1,15 @@
+module sync_3bit_down_counter(
+    input clk,
+    output reg [2:0] Q
+);
+
+initial begin
+   Q = 3'b111;
+end
+
+always @(posedge clk) begin
+   Q <= Q + 1;
+end
+
+endmodule
 
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/02-Three-Bit-Synchoronous-Down-Counter/three_bit_synchoronous_down_counter_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/02-Three-Bit-Synchoronous-Down-Counter/three_bit_synchoronous_down_counter_tb.v
@@ -1,1 +1,25 @@
+module sync_3bit_up_counter_tb;
+  reg clk;
+  wire [2:0] Q;
+
+  sync_3bit_up_counter DUT(
+    .clk(clk),
+    .Q(Q)
+  );
+
+  initial begin
+    clk = 0;
+    forever #5 clk <= ~clk;
+  end
+
+  initial begin
+    $dumpfile("sync_3bit_up_counter.vcd");
+    $dumpvars(0, sync_3bit_up_counter_tb);
+
+    #100;
+    $finish;
+  end
+endmodule  
+
+
 
