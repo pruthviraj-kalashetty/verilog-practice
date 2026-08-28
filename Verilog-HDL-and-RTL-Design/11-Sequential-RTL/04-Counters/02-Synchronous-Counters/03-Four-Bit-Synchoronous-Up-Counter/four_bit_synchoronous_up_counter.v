b8aac0199830fb6e0ed75a966843055a929cd4f6
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-08-28 14:08:39 +0530

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/03-Four-Bit-Synchoronous-Up-Counter/four_bit_synchoronous_up_counter.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/03-Four-Bit-Synchoronous-Up-Counter/four_bit_synchoronous_up_counter.v
@@ -1,1 +1,15 @@
 
+module sync_4bit_up_counter(
+    input clk,
+    output reg [3:0] Q
+);
+
+initial begin
+   Q = 4'b0000;
+end
+
+always @(posedge clk) begin
+   Q <= Q + 1;
+end
+
+endmodule
--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/03-Four-Bit-Synchoronous-Up-Counter/four_bit_synchoronous_up_counter_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/02-Synchronous-Counters/03-Four-Bit-Synchoronous-Up-Counter/four_bit_synchoronous_up_counter_tb.v
@@ -1,1 +1,25 @@
+module sync_4bit_up_counter_tb;
+  reg clk;
+  wire [2:0] Q;
+
+  sync_4bit_up_counter DUT(
+    .clk(clk),
+    .Q(Q)
+  );
+
+  initial begin
+    clk = 0;
+    forever #5 clk <= ~clk;
+  end
+
+  initial begin
+    $dumpfile("sync_4bit_up_counter.vcd");
+    $dumpvars(0, sync_4bit_up_counter_tb);
+
+    #1000;
+    $finish;
+  end
+endmodule  
+
+
 
