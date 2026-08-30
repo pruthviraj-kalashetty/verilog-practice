c51c6c0b2356bac9d8ba7fa842de07b77fed45c8
Pruthviraj Kalashetty <pruthvirajak7171@gmail.com>
2026-08-30 09:50:48 +0000

    added tb

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/03-Ring-Counter/ring_counter_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/03-Ring-Counter/ring_counter_tb.v
@@ -1,1 +1,23 @@
-module
+module ring_counter_4bit_tb;
+  reg clk;
+  wire [3:0] Q;
+
+  ring_counter_4bit DUT(
+    .clk(clk)'
+    .Q(Q),
+  );
+
+initial begin
+  clk = 0;
+  forever #5 clk <= ~clk;
+end  
+
+initial begin 
+  $dumpfile("ring_counter_4bit.vcd");
+  $dumpvers(0, ring_counter_4bit_tb);
+
+  #100;
+
+  $finish;
+end
+endmodule  
