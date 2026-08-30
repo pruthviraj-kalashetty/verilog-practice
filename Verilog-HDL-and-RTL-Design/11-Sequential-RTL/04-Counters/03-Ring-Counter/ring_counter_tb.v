f4cbf4f123d128ec901098dad1f97b80becb8569
Pruthviraj Kalashetty <pruthvirajak7171@gmail.com>
2026-08-30 10:13:21 +0000

    updated

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/03-Ring-Counter/ring_counter_tb.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/03-Ring-Counter/ring_counter_tb.v
@@ -1,23 +1,27 @@
 module ring_counter_4bit_tb;
-  reg clk;
-  wire [3:0] Q;
 
-  ring_counter_4bit DUT(
-    .clk(clk)'
-    .Q(Q),
-  );
+reg clk;
+wire [3:0] Q;
+
+ring_counter_4bit DUT(
+    .clk(clk),
+    .Q(Q)
+);
+
+// Clock
 
 initial begin
-  clk = 0;
-  forever #5 clk <= ~clk;
-end  
-
-initial begin 
-  $dumpfile("ring_counter_4bit.vcd");
-  $dumpvers(0, ring_counter_4bit_tb);
-
-  #100;
-
-  $finish;
+    clk = 0;
+    forever #5 clk <= ~clk;
 end
-endmodule  
+
+initial begin
+    $dumpfile("ring_counter_4bit.vcd");
+    $dumpvars(0, ring_counter_4bit_tb);
+
+    #100;
+
+    $finish;
+end
+
+endmodule
