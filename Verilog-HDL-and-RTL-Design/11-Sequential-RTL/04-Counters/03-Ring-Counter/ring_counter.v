c41e8c23a33a47753d8889aa17bf13abcdc65fb7
Pruthviraj Kalashetty <pruthvirajak7171@gmail.com>
2026-08-30 09:11:34 +0000

    updated verilog

--- Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/03-Ring-Counter/ring_counter.v
+++ Verilog-HDL-and-RTL-Design/11-Sequential-RTL/04-Counters/03-Ring-Counter/ring_counter.v
@@ -1,1 +1,14 @@
-module ring counter()
+module ring_counter_4bit(
+    input clk,
+    output reg [3:0] Q
+);
+
+initial begin
+    Q = 4'b1000;
+end
+
+always @(posedge clk) begin
+    Q <= {Q[2:0], Q[3]};
+end
+
+endmodule
