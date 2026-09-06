c419d5a51236d41a27c02c91065744772f027bff
Pruthviraj Kalashetty <pruthvirajak7171@gmail.com>
2026-09-06 11:03:55 +0000

    added tb

---
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/03-Mealy-FSM/mealy_fsm_tb.v
@@ -0,0 +1,37 @@
+module moore_fsm_tb;
+  reg clk;
+  reg x;
+
+  wire y;
+
+   moore_fsm DUT(
+    .clk(clk),
+    .x(x),
+    .y(y)
+   );
+
+initial begin
+  clk = 0;
+  forever #5 clk <= ~clk;
+end
+
+initial begin
+  $dumpfile("moore_fsm.vcd");
+  $dumpvars(0, moore_fsm_tb);
+
+end
+
+initial begin
+    x = 0;
+
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 0;
+
+    #10;
+    $finish;
+end
+
+endmodule
