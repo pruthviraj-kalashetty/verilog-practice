891c62c3960c7d426f838a2a0928ee563ed48762
Pruthviraj Kalashetty <pruthvirajak7171@gmail.com>
2026-09-08 07:37:17 +0000

    verilog

---
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/04-Sequence-detector/02-Sequence-Detector-1011-Non-Overlapping/sequence_detector_1011_non_overlap.v
@@ -0,0 +1,60 @@
+module sequence_detector_1011_non_overlap(
+    input clk,
+    input x,
+    output y
+);
+
+reg [2:0] state;
+reg [2:0] next_state;
+
+always @(posedge clk) begin
+  state <= next_state;
+end
+
+always @(*) begin
+   if (state == 3'b000) begin
+       if (x == 0)
+         next_state = 3'b000;
+       else 
+         next_state = 3'b001;
+   end
+
+   else if (state == 3'b001) begin
+      if (x == 0)
+        next_state = 3'b010;
+      else 
+          next_state = 3'b001;
+    end
+
+    else if (state == 3'b010) begin
+       if (x == 0)
+          next_state = 3'b000;
+        else 
+          next_state = 3'b011;
+    end             
+
+    else if (state == 3'b011) begin
+        if (x == 0)
+           next_state = 3'b000;
+        else 
+           next_state = 3'b100;
+    end
+
+    else if (state == 3'b100) begin
+        if (x == 0)
+           next_state = 3'b000;
+        else 
+          next_state = 3'b000;
+    end                        
+end
+
+always @(*) begin
+  if (state == 3'b100)
+     y = 1;
+  else  
+     y = 0;
+end
+endmodule 
+
+
+  
