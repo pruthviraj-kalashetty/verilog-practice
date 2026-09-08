1d2516ada5f2a1bcefbbd831a6bbeeffe84af707
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-09-08 13:26:40 +0530

    Update sequence_detector_1011_overlap_tb.v

--- Verilog-HDL-and-RTL-Design/12-RTL-FSM/04-Sequence-detector/01-Sequence-Detector-1011-Overlapping/sequence_detector_1011_overlap_tb.v
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/04-Sequence-detector/01-Sequence-Detector-1011-Overlapping/sequence_detector_1011_overlap_tb.v
@@ -1,10 +1,10 @@
-module sequnece_detector_1011_overlap_tb;
+module sequence_detector_1011_overlap_tb;
    reg clk;
    reg x;
 
    wire y;
 
-   sequnece_detector_1011_overlap DUT(
+   sequence_detector_1011_overlap DUT(
     .clk(clk),
     .x(x),
     .y(y)
@@ -16,8 +16,8 @@ initial begin
 end 
 
 initial begin
-   $dumpfile("sequnece_detector_1011_overlap.vcd");
-   $dumpvars(0, sequnece_detector_1011_overlap_tb);
+   $dumpfile("sequence_detector_1011_overlap.vcd");
+   $dumpvars(0, sequence_detector_1011_overlap_tb);
 end
 
 initial begin
