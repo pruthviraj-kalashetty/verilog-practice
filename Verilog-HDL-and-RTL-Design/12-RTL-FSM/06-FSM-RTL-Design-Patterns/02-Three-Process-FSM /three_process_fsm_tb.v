daee118565b53d616ad5ae47845c9f7ca639d07e
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-09-09 14:49:02 +0530

    Update three_process_fsm_tb.v

--- Verilog-HDL-and-RTL-Design/12-RTL-FSM/06-FSM-RTL-Design-Patterns/02-Three-Process-FSM /three_process_fsm_tb.v
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/06-FSM-RTL-Design-Patterns/02-Three-Process-FSM /three_process_fsm_tb.v
@@ -1,1 +1,36 @@
+module three_process_fsm_tb;
 
+reg clk;
+reg x;
+wire y;
+
+three_process_fsm DUT(
+    .clk(clk),
+    .x(x),
+    .y(y)
+);
+
+initial begin
+    clk = 0;
+    forever #5 clk <= ~clk;
+end
+
+initial begin
+    $dumpfile("three_process_fsm.vcd");
+    $dumpvars(0, three_process_fsm_tb);
+end
+
+initial begin
+    x = 0;
+
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 0;
+    #10;
+
+    $finish;
+end
+
+endmodule
