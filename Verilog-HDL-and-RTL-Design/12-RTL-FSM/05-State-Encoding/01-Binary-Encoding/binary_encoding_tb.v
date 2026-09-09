fc9d83137cf69d04b1d398f9d09f5849fa4f678f
Pruthviraj Kalashetty <pruthvirajak7171@gmail.com>
2026-09-09 06:31:22 +0000

    added verilog

---
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/05-State-Encoding/01-Binary-Encoding/binary_encoding_tb.v
@@ -0,0 +1,36 @@
+module binary_encoding_tb;
+
+reg clk;
+reg x;
+wire y;
+
+binary_encoding DUT(
+    .clk(clk),
+    .x(x),
+    .y(y)
+);
+
+initial begin
+    clk = 0;
+    forever #5 clk <= ~clk;
+end
+
+initial begin
+    $dumpfile("binary_encoding.vcd");
+    $dumpvars(0, binary_encoding_tb);
+end
+
+initial begin
+    x = 0;
+
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 0;
+    #10;
+
+    $finish;
+end
+
+endmodule
