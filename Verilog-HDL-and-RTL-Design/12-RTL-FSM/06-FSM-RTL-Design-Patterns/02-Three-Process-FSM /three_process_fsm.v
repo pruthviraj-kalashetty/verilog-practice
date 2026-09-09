e5a7ad49e2d8850deb2d5af922c9068ad7fbf765
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-09-09 14:48:37 +0530

    Update three_process_fsm.v

--- Verilog-HDL-and-RTL-Design/12-RTL-FSM/06-FSM-RTL-Design-Patterns/02-Three-Process-FSM /three_process_fsm.v
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/06-FSM-RTL-Design-Patterns/02-Three-Process-FSM /three_process_fsm.v
@@ -1,1 +1,43 @@
+module three_process_fsm(
+    input clk,
+    input x,
+    output reg y
+);
 
+reg state;
+reg next_state;
+
+initial begin
+    state = 0;
+end
+
+// Process 1: State Register
+always @(posedge clk) begin
+    state <= next_state;
+end
+
+// Process 2: Next-State Logic
+always @(*) begin
+    if (state == 0) begin
+        if (x == 0)
+            next_state = 0;
+        else
+            next_state = 1;
+    end
+    else begin
+        if (x == 0)
+            next_state = 0;
+        else
+            next_state = 1;
+    end
+end
+
+// Process 3: Output Logic
+always @(*) begin
+    if (state == 1)
+        y = 1;
+    else
+        y = 0;
+end
+
+endmodule
