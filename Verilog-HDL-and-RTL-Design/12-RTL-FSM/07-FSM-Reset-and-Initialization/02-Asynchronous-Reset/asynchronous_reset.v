fe79ab7790853abc754157f1f301515559ef5980
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-09-10 10:01:55 +0530

    Update asynchronous_reset.v

---
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/07-FSM-Reset-and-Initialization/02-Asynchronous-Reset/asynchronous_reset.v
@@ -0,0 +1,47 @@
+module asynchronous_reset(
+    input clk,
+    input reset,
+    input x,
+    output reg y
+);
+
+reg state;
+reg next_state;
+
+initial begin
+    state = 0;
+end
+
+// Process 1: State Register
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        state <= 0;
+    else
+        state <= next_state;
+end
+
+// Process 2: Next-State Logic
+always @(*) begin
+    if (state == 0) begin
+        if (x == 0)
+            next_state = 0;
+        else
+            next_state = 1;
+    end
+    else begin
+        if (x == 0)
+            next_state = 0;
+        else
+            next_state = 1;
+    end
+end
+
+// Process 3: Output Logic
+always @(*) begin
+    if (state == 1)
+        y = 1;
+    else
+        y = 0;
+end
+
+endmodule
