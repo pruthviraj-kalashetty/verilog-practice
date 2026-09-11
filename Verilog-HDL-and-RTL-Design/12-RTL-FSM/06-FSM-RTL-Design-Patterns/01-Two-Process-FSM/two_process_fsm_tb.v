e1787add0e65099b84b85ae2834ffb7b58a54478
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-09-11 13:51:23 +0530

    Update two_process_fsm_tb.v

--- Verilog-HDL-and-RTL-Design/12-RTL-FSM/06-FSM-RTL-Design-Patterns/01-Two-Process-FSM/two_process_fsm_tb.v
+++ Verilog-HDL-and-RTL-Design/12-RTL-FSM/06-FSM-RTL-Design-Patterns/01-Two-Process-FSM/two_process_fsm_tb.v
@@ -1,10 +1,10 @@
-module gray_encoding_tb;
+module two_process_fsm_tb;
 
 reg clk;
 reg x;
 wire y;
 
-gray_encoding DUT(
+two_process_fsm DUT(
     .clk(clk),
     .x(x),
     .y(y)
@@ -16,8 +16,8 @@ initial begin
 end
 
 initial begin
-    $dumpfile("gray_encoding.vcd");
-    $dumpvars(0, gray_encoding_tb);
+    $dumpfile("two_process_fsm.vcd");
+    $dumpvars(0, two_process_fsm_tb);
 end
 
 initial begin
