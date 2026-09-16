6a3c2c1b9a8257196bb15d833a2ac30a96b8b9ae
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-09-16 13:39:57 +0530

    updated

--- RTL-Mini-Projects/01-Traffic-Light-Controller/03-rtl-tb/traffic_light_controller_tb.v
+++ RTL-Mini-Projects/01-Traffic-Light-Controller/03-rtl-tb/traffic_light_controller_tb.v
@@ -1,13 +1,16 @@
-`timescale 1ns\1ps
+`timescale 1ns/1ps
 
 module traffic_light_controller_tb;
   
+  // Testbench Signals
   reg clk;
   reg reset;
   
+  // DUT Output Signals
   wire [2:0] north_south;
   wire [2:0] east_west;
 
+  // DUT Connetions
   traffic_light_controller dut(
     .clk(clk),
     .reset(reset),
@@ -15,22 +18,26 @@ module traffic_light_controller_tb;
     .east_west(east_west)
   );
 
+// Clock Generation
 initial begin 
     clk = 1'b0;
-    forever #5 clk ~clk;
+    forever #5 clk = ~clk;
 end    
 
+// Reset
 initial begin
     reset = 1'b1;
     #10
     reset = 1'b0;
 end    
 
+// Waveform dump
 initial begin
-    $dumpfile(traffic_light_controller.vcd);
+    $dumpfile("traffic_light_controller.vcd");
     $dumpvars(0, traffic_light_controller_tb);
 end 
 
+//End simulation
 initial begin 
     #100;
     $finish;
