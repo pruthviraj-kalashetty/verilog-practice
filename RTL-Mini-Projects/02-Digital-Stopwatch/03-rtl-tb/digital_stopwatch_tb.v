0ef09e810ad206bb20eec6eebe474af52c212e38
Pruthviraj Kalashetty <109294152+pruthviraj-kalashetty@users.noreply.github.com>
2026-09-20 11:52:52 +0530

    updated

---
+++ RTL-Mini-Projects/02-Digital-Stopwatch/03-rtl-tb/digital_stopwatch_tb.v
@@ -0,0 +1,39 @@
+module digital_stopwatch_tb;
+
+    // Testbench input signals
+    reg clk;
+    reg reset;
+    reg start;
+    
+    //DUT output signals
+    wire [7:0] minutes;
+    wire [5:0] seconds;
+    wire [6:0] subseconds;
+ 
+    // Connect testbench to DUT
+    digital_stopwatch dut(
+        .clk(clk),
+        .reset(reset),
+        .start(start),
+        .minutes(minutes),
+        .seconds(seconds),
+        .subseconds(subseconds)
+    );
+  // Generate clock
+  initial begin 
+    clk = 1'b0;
+    forever #5 clk = ~ clk;
+  end
+
+  // Create waveform file
+  initial begin
+      $dumpfile("digital_stopwatch.vcd");
+      $dumpvars(0, digital_stopwatch_tb);
+  end
+  
+  // Stop simulation
+  initial begin 
+      #100;
+      $finish;
+  end
+endmodule     
